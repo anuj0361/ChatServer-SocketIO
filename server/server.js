6542adcaff04fcd03513b2a37cdc2ded2ce2bbd5
unknown <anujpandey1630@gmail.com>
2018-09-16 21:09:14 +0530

    New user changes

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from:'Admin',
+    text:'Welcome to chatroom',
+    createAt:new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from:'Admin',
+    text:'New User joined',
+    createAt:new Date().getTime()
+  });
+
   socket.on('createMessage', (message)=>{
     console.log('createMessage', message);
     io.emit('newMessage', {
