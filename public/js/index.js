870e26942ff6f43d9285d1e1941fe89fa97c5c37
unknown <anujpandey1630@gmail.com>
2018-09-16 20:30:27 +0530

    create Message

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket=io();
 
 socket.on('connect', function () {
   console.log('connected to server');
-
-  socket.emit('createMessage', {
-    from: 'anj',
-    text: 'yessss',
-  });
-
 });
 
 socket.on('disconnect',function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
-  socket.emit('newMessage',{
-    from:'xyz',
-    text:'whts up?',
-    createAt:34342
-  });
-
   socket.on('createMessage', (message)=>{
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from:message.from,
+      text:message.text,
+      createAt:new Date().getTime()
+    });
   });
 
   socket.on('disconnect',()=>{
