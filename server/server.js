6d40e2e87250cbbf8f62bfeae098cc7b3fc63735
unknown <anujpandey1630@gmail.com>
2018-09-16 20:09:26 +0530

    Add & create message event

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
+  socket.emit('newMessage',{
+    from:'xyz',
+    text:'whts up?',
+    createAt:34342
+  });
+
+  socket.on('createMessage', (message)=>{
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect',()=>{
     console.log('User disconnected');
   });
