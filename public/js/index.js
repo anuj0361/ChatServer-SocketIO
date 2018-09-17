cf123d9f005d2055ee7e4228a91142d3c8d1eedb
unknown <anujpandey1630@gmail.com>
2018-09-18 00:07:52 +0530

    Sending Geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,17 @@ socket.on('newMessage', function(message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message) {
+
+  var li=jQuery('<li></li>');
+  var  a=jQuery('<a target="_blank">My currernt location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
@@ -23,5 +34,21 @@ jQuery('#message-form').on('submit', function(e) {
     text:jQuery('[name=message]').val()
   }, function (data) {
     console.log(data)
+  });
+});
+
+var locationButton=jQuery('#send-location');
+locationButton.on('click', function (){
+  if(!navigator.geolocation) {
+    return alert('Geolocation not supported');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude:position.coords.latitude,
+      longitude:position.coords.longitude
+    });
+  }, function (){
+    alert('Unable to fetch position');
   });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,16 +1,27 @@
 var expect=require('expect');
-var {generateMessage}=require('./message');
+var {generateMessage,generateLocationMessage}=require('./message');
 
 describe('generateMessage', ()=>{
   it('should generate correct message object', ()=>{
     var from='anj';
     var text='somthng';
     var message=generateMessage(from,text);
-    
+
     expect(message.createdAt).toBeA('number');
-    expect(message).toInclude({
-      from,
-      text
-    });
+    expect(message).toInclude({from,text});
+  });
+});
+
+
+describe('generateLocationMessage', ()=>{
+  it('should generate correct location object', ()=>{
+    var from='anj12';
+    var latitude='10';
+    var longitude='15';
+    var url='https://www.google.com/maps?q=10,15'
+    var message=generateLocationMessage(from,latitude,longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from,url});
   });
 });
